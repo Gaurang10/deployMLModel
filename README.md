4680817b3e53f93d2d2605d093a2c33d426271df
Gaurang <gaurangs@koinearth.com>
2020-01-13 15:56:06 +0530

    "classification model"

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # deployMLModel
 This repo contains a sample flask server with an API to get prediction using Machine learning model. 
+
+
+Step 1: Create python virtual enviroment. 
+```
+sudo pip3 install virtualenv 
+virtualenv venv
+source venv/bin/activate 
+```
---
+++ app.py
@@ -0,0 +1,33 @@
+# load Flask 
+import pickle
+import numpy as np
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+model = None
+
+def load_model():
+    global model
+    # model variable refers to the global variable
+    with open('iris_trained_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+# define a predict function as an endpoint 
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    data = {"success": False}
+    if request.method == 'POST':
+        params = request.get_json()  # Get data posted as a json
+        if (params != None):
+            params = np.array(params)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
+            prediction = model.predict(params)
+            data["response"] = prediction[0]
+            data["success"] = True
+        print("Respose data", data)
+    return str(data)
+  
+# start the flask app, allow remote connections
+if __name__ == '__main__':
+    load_model()  # load model at the beginning once only
+    app.run(host='0.0.0.0', port=8080)
